07d65ad8e539c83259c64db78bb78f49aed51b92
Tgithinji <t.muraguri420@gmail.com>
2024-03-26 15:43:43 +0300

    Function that lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env/python3
+"""
+8-all module
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+    """
+    list_docs = []
+
+    for doc in mongo_collection.find():
+        list_docs.append(doc)
+
+    return list_docs
